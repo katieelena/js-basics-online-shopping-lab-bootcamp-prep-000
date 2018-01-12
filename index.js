3b464f7b82008e80ac2d3068a2062afd7b580ac3
Kaitlyn Robey <katie.elena2000@gmail.com>
2018-01-12 13:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length == 0) {
+  if (cart.length = 0) {
     var cartItems = Object.keys(cart)
     var cartPhrase = []
     for (var i = 0; i < cartItems.length; i++) {
